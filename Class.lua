7a3caa24a3c11890d62bfe1344e37bae555e0411
Jordan Irwin <antumdeluge@gmail.com>
2026-06-03 15:41:12 -0700

    Global function for creating new classes

--- Class.lua
+++ Class.lua
@@ -31,3 +31,43 @@ local ClassMeta = {
 		return "foobar"
 	end
 }
+
+
+--- Function called to create a new class table.
+--
+--  After creation, class can then be instantiated with `ClassName([<args>])`. Can be called with
+--  the following argument options.
+--
+--  - NewClass(): Creates a bare-minimum new class without inheritence.
+--  - NewClass(template): Creates a new class from `template` without inheritence.
+--  - NewClass(parent, template): Creates a new class from `template` with inheritence from
+--    `parent`. Same as `NewClass(nil, template)`.
+--
+--  @param parent
+--    The parent class that is being inherited.
+--  @param template
+--    Table template of new class.
+NewClass = function(parent, template)
+	-- allow calling with class template table as single argument
+	if template == nil then
+		template = parent
+		parent = nil
+	end
+
+	local class = template or {}
+	class.__index = class
+
+	if parent ~= nil then
+		-- preserves inheritence
+		setmetatable(class, {__index = parent, __call = ClassMeta.__call})
+	else
+		setmetatable(class, ClassMeta)
+	end
+
+	if class.__init == nil then
+		-- default constructor (must be added after `setmetatable`)
+		class.__init = function(self) end
+	end
+
+	return class
+end
