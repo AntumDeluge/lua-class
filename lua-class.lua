2107eed92609e242b60c5541c2d96cb784da7f5c
Jordan Irwin <antumdeluge@gmail.com>
2026-06-07 19:12:03 -0700

    Rework

--- lua-class.lua
+++ lua-class.lua
@@ -13,6 +13,8 @@
 --  This table serves as a template for creating a constructor for classes to be instantiated. The
 --  class must have a constructor function named "__init" which is called during instantiation.
 local ClassMeta = {
+	__type = "class",
+
 	--- Meta constructor.
 	--
 	--  Called during instantiation to create a new object.
@@ -23,10 +25,26 @@ local ClassMeta = {
 	--    All parameters that should be passed to the class constructor.
 	__call = function(class, ...)
 		local instance = setmetatable({}, class)
+		local mt = getmetatable(instance)
+		mt.__type = "class_object"
+		mt.__tostring = getmetatable(class).__tostring
 		class.__init(instance, ...)
+		local parent = class:getParentClass()
 		return instance
+	end,
+
+	__tostring = function(class)
+		return type(class)..": "..string.format("%p", class)
 	end
 }
+
+
+local current_uuid = 1000
+local nextClassUUID = function(class)
+	uuid = current_uuid
+	current_uuid = current_uuid + 1
+	return uuid
+end
 
 
 --- Function called to create a new class table.
@@ -49,16 +67,25 @@ NewClass = function(parent, template)
 		template = parent
 		parent = nil
 	end
+	-- ensure sub-class of Class
+	parent = parent or Class
 
-	local class = template or Class or {}
+	local class = {}
+	if type(template) == "table" or type(template) == "class" and template ~= parent then
+		for k, v in pairs(template) do
+			class[k] = v
+		end
+	end
 	class.__index = class
 
-	if parent ~= nil then
-		-- preserves inheritence
-		setmetatable(class, {__index = parent, __call = ClassMeta.__call})
-	else
-		setmetatable(class, ClassMeta)
+	local cm = {
+		__index = parent,
+		__uuid = nextClassUUID()
+	}
+	for k, v in pairs(ClassMeta) do
+		cm[k] = v
 	end
+	setmetatable(class, cm)
 
 	if class.__init == nil then
 		-- default constructor (must be added after `setmetatable`)
@@ -71,12 +98,55 @@ end
 
 --- Base class for all new classes to inherit.
 Class = NewClass({
-	--- Data type property to differentiate between "table".
-	__type = "class",
+	--- Retrieves class's unique identifier.
+	--
+	--  @return
+	--    UUID.
+	getClassUUID = function(self)
+		mt = getmetatable(self:getClass())
+		return mt and mt.__uuid
+	end,
 
-	--- Overrides `table.__tostring` function to customize output for `class`.
-	__tostring = function(self)
-		return self.__type..": "..string.format("%p", self)
+	--- Retreives the parent class of this class or instance.
+	--
+	--  @return
+	--    Class that this inherits or `nil`.
+	getParentClass = function(self)
+		local mt = self and getmetatable(self)
+		return mt and mt.__index or nil
+	end,
+
+	--- Retrievies class of this instance.
+	--
+	--  @return
+	--    Class implementation.
+	getClass = function(self)
+		if type(self) == "class" then
+			return self
+		elseif type(self) == "class_object" then
+			return self:getParentClass()
+		end
+	end,
+
+	--- Checks if this class is an instance of another.
+	--
+	--  @param other
+	--    Class from which to look for inheritence.
+	instanceof = function(self, other)
+		if type(self) ~= "class_object" or type(other) ~= "class" then
+			return false
+		end
+
+		local target_uuid = other:getClassUUID()
+		local current = self:getClass()
+		while current do
+			uuid = current:getClassUUID()
+			if uuid == target_uuid then
+				return true
+			end
+			current = current:getParentClass()
+		end
+		return false
 	end
 })
 
